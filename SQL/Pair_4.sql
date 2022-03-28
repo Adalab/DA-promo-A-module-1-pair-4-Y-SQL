f59cf43ca39dee879d08b501b6860efcfd20dea8
Almudena <al.garciaperucha@gmail.com>
2022-03-28 14:45:09 +0200

    entrega final

--- SQL/Pair_4.sql
+++ SQL/Pair_4.sql
@@ -1,41 +1,41 @@
-USE Northwind;
+USE northwind;
 
 /*1 productos + baratos y caros*/
-SELECT MIN(UnitPrice) AS 'LowestPrice'
-FROM Products;
+SELECT MIN(unit_price) AS 'lowest_price'
+FROM products;
 
-SELECT MAX(UnitPrice) AS 'highestPrice'
-FROM Products;
+SELECT MAX(unit_price) AS 'highest_price'
+FROM products;
 
 /*2  precio medio de todos los productos*/
-SELECT AVG(UnitPrice) AS 'preciomedio'
-FROM Products;
+SELECT AVG(unit_price) AS 'precio_medio'
+FROM products;
 
 /*3 Carga máxima y mínima de los pedidos de UK*/
-SELECT MIN(Freight)
-FROM Orders
-WHERE ShipCountry = 'UK';
+SELECT MIN(freight)
+FROM orders
+WHERE ship_country = 'UK';
 
-SELECT MAX(Freight)
-FROM Orders
-WHERE ShipCountry = 'UK';
+SELECT MAX(freight)
+FROM orders
+WHERE ship_country = 'UK';
 
-/*4 productos que se venden por encima de preciomedio*/
-SELECT AVG(UnitPrice) AS 'preciomedio'
-FROM Products;
+/*4 productos que se venden por encima de precio medio*/
+SELECT AVG(unit_price) AS 'precio_medio'
+FROM products;
 
-SELECT ProductID, ProductName, Unitprice
-FROM Products
-WHERE UnitPrice >= 'preciomedio';
+SELECT product_id, product_name, unit_price
+FROM products
+WHERE unit_Price >= 'precio_medio';
 
 /*5 productos descontinuados*/
-SELECT COUNT(ProductID)
-FROM Products
-WHERE Discontinued = '1';
+SELECT COUNT(product_id)
+FROM products
+WHERE discontinued = '1';
 
-/*6 productid y productname de productos no descontinuados; solo 10 con id + elevado*/
-SELECT ProductID, ProductName
-FROM Products
-WHERE NOT Discontinued = '1'
-ORDER BY ProductID DESC
+/*6 product id y product name de productos no descontinuados; solo 10 con id + elevado*/
+SELECT product_id, product_name
+FROM products
+WHERE NOT discontinued = '1'
+ORDER BY product_id DESC
 LIMIT 10;
